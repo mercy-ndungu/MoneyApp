446714bd88cb57a73d56778483cf42b2c7cc374d
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-05-08 10:52:20 +0300

    first commit

---
+++ app/src/main/java/com/example/moneyapp/SendMoneyProcess.kt
@@ -0,0 +1,14 @@
+package com.example.moneyapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SendMoneyProcess : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_money_process)
+
+    }
+}
